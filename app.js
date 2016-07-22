a0afd4482249a3feaa2baedfc49540288f0e4b0b
abby-garcia <acg361@gmail.com>
2016-07-22 16:37:04 -0400

    rewrote code

--- app.js
+++ app.js
@@ -1,13 +1,55 @@
 const ProductList = React.createClass({  //using React.createClass creates new components
-	render : function(){ // it passes, a single arguments, a js OBJECT!
+	render : function() { // it passes, a single arguments, a js OBJECT!
+		const product = Data[0];
 		return (
-			<div className = 'ui items'>
-			Hello, friend! I am a basic React component.
-			</div> /
-			);
+			<div className='ui items'>
+				<Product 
+					id={product.id}
+					title={product.title}
+					description={product.description}
+					url={product.url}
+					votes={product.votes}
+					submitter_avatar_url={product.submitter_avatar_url}
+					product_image_url={product.product_image_url}
+				/> 
+			</div> 
+		);
 	},
 });
 
+
+
+const Product = React.createClass({
+	render: function () {
+		return(
+			<div className = 'item'>
+				<div className = 'image'>
+					<img src ={this.props.product_image_url} />
+				</div>
+			<div className='middle aligned content'>
+				<div className='header'>
+				 <a>
+				 	<i className = 'large caret up icon'></i>
+				 </a>
+				 {this.props.votes}
+				</div>
+				<div className='description'>
+					<a href={this.props.url}>
+						{this.props.title}
+					</a>
+				</div>
+				<div classNme='extra'>
+					<span>Submitted by: </span>
+					<img 
+						className='ui avatar image'
+						src={this.props.submitter_avatar_url}
+					/>
+				</div>
+			</div>
+		</div>
+		);				
+	},
+});		
 
 
 
